470d502336954a1679225892b4d4a42ad979ee1f
jose blois <joseblois3@gmail.com>
2020-03-08 21:06:55 -0300

    Clase 3 practica extra

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+var a = 5;
+var b = 2;
+alert( 'a = '+a+ ' b = '+b+' Suma: '+(a+b)+' Resta: '+(a-b)+' Multiplicación: '+(a*b)+' División: '+(a/b)+' Modulo: '+(a%b));
+var dias = 4563;
+console.log('Viviste aproximadamente unos '+ parseInt(dias / 365)+' años');
+console.log('Viviste aproximadamente unos '+ parseInt(dias / 365)+ ' años y '+(dias % 365 ) +' dias ')
+var meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+alert(meses[4]+' '+meses[10]);
+var v = [true, false,0,"hola","adios", 4, 2];
+var c = v[5];
+var d = v[6];
+console.log('suma: ' + (c+d) + ', resta: '+(c-d)+', multiplicación: '+(c*d)+', división: '+(c/d)+', modulo: '+(c%d) );
+var e = v[1]
+var f = v[2]
+console.log(e == f);
+console.log(e === f);
+
